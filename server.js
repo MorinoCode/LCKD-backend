03c3cd313df6ebd52f25b635af6449ee1148924a
MorinoCode <mortezamastercode@gmail.com>
2025-10-22 21:56:09 +0200

    test workflow

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT
 const app = express();
 app.use(express.json());
 
+app.get('/', (req , res) => {
+    res.send('Hi')
+})
+
 
 app.listen(PORT , ()=>{
 console.log(`App is runnig on : ${PORT}`);
