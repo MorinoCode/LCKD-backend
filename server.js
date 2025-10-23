b7d0c3dc9475a23e4a7dc5793f881f1e70bffaab
MorinoCode <mortezamastercode@gmail.com>
2025-10-23 13:31:05 +0200

    fix : cors

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ import passwordRoutes from "./routes/passwordRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 
-
-
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -16,13 +14,21 @@ const app = express();
 
 app.use(express.json());
 app.use(logger);
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://lckd-bucket-dev.s3-website.eu-north-1.amazonaws.com",
+      "https://lckd-bucket-dev.s3-website.eu-north-1.amazonaws.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/passwords", passwordRoutes);
 app.use("/api/users", userRoutes);
-
 
 // Error handling middleware
 app.use(errorHandler);
